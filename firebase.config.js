568269cfd129e812b665df4e653c0bda5af8d3a3
cvhlpont <pontchvl@gmail.com>
2024-05-09 00:04:04 +0200

    update: added firebase to project

---
+++ firebase.config.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: NEXT_PUBLIC_API_DOMAIN,
+  projectId: NEXT_PUBLIC_API_PROJECT_ID,
+  storageBucket: NEXT_PUBLIC_API_STORAGE_BUCKET,
+  messagingSenderId: NEXT_PUBLIC_API_MESSAGING_SENDER_ID,
+  appId: NEXT_PUBLIC_API_APP_ID,
+};
+
+// Initialize Firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+// Communicates with nosql database
+const db = getFireStore(app);
+
+// Do we need storage?
+const storage = getStorage(app);
+
+export { db, storage };
