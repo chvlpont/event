cfe9acdcd5d02dfb001a9209ec8d8e4b8fceeade
Oscar <oscar_karlsson@live.se>
2024-05-10 17:37:08 +0200

    Updated Sidebar component to handle submenu visibility and active page state

--- app/admin/_components/sidebar.jsx
+++ app/admin/_components/sidebar.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState, useEffect } from 'react'
 import { FaUser, FaCog, FaCalendarCheck, FaCalendarPlus, FaEdit, FaPaintBrush, FaAngleDoubleRight, FaAngleDoubleLeft, FaChevronDown, FaChevronRight, FaSignOutAlt  } from 'react-icons/fa'
 import { MdDashboard } from "react-icons/md";
@@ -6,6 +7,7 @@ function Sidebar({ setSelectedPage }) {
   const [showEventOptions, setShowEventOptions] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(false); // default to false
   const [activePage, setActivePage] = useState('Dashboard');
+  const [activeSubPage, setActiveSubPage] = useState('');
 
 
   useEffect(() => {
@@ -13,12 +15,19 @@ function Sidebar({ setSelectedPage }) {
     setIsCollapsed(window.innerWidth <= 640);
   }, []);
 
-  const handlePageSelect = (page) => { 
-    setSelectedPage(page);
-    setActivePage(page);
-    if (page === 'CreateEvent' || page === 'EditEvent') {
-      setShowEventOptions(true);
+  // handlePageSelect function
+  const handlePageSelect = (page, subPage) => {
+    if (subPage) {
+      setSelectedPage(subPage);
+      setActiveSubPage(subPage);
     } else {
+      setSelectedPage(page);
+      setActiveSubPage('');
+    }
+    setActivePage(page); 
+    if (page === 'Event' && !subPage) {
+      setShowEventOptions(!showEventOptions);
+    } else if (page !== 'Event') {
       setShowEventOptions(false);
     }
   };
@@ -47,7 +56,7 @@ function Sidebar({ setSelectedPage }) {
   return (
     <div className={`h-screen bg-gray-800 text-white flex flex-col items-start justify-start pt-5 transition-all duration-500 ease-in-out ${isCollapsed ? 'w-20' : 'w-64'}`}>
       <div className="flex items-center w-full mb-2 p-4 justify-between">
-        {!isCollapsed && <h1 className="h-8 w-auto text-white text-center text-2xl font-bold tracking-wide ">NexusPanel</h1>} 
+        {!isCollapsed && <h1 className="h-8 w-auto text-white text-center text-2xl font-bold tracking-wide ">AdminPanel</h1>} 
         {isCollapsed 
           ? <FaAngleDoubleRight className="text-white mr-2 cursor-pointer" onClick={() => setIsCollapsed(!isCollapsed)} onKeyDown={(event) => handleKeyDown(event, () => setIsCollapsed(!isCollapsed))} tabIndex="0" role="button" aria-label="Expand Sidebar" /> 
           : <FaAngleDoubleLeft className="text-white mr-2 cursor-pointer" onClick={() => setIsCollapsed(!isCollapsed)} onKeyDown={(event) => handleKeyDown(event, () => setIsCollapsed(!isCollapsed))} tabIndex="0" role="button" aria-label="Collapse Sidebar" />}
@@ -66,14 +75,19 @@ function Sidebar({ setSelectedPage }) {
       </div>
       {showEventOptions && (
         <>
-<div className={`cursor-pointer flex items-center w-full mb-2 hover:bg-gray-700 p-4 pl-8 ${activePage === 'CreateEvent' ? 'bg-gray-700 text-white border-l-4 border-green-500' : ''}`} onClick={() => handlePageSelect('CreateEvent')} onKeyDown={(event) => handleKeyDown(event, () => handlePageSelect('CreateEvent'))} tabIndex="0" role="button" aria-label="Create Event">
-  <FaCalendarPlus className="text-white mr-2" />
-  {!isCollapsed && <p className="text-white">Create Event</p>}
+<div className="cursor-pointer flex items-center w-full mb-2 hover:bg-gray-700 p-4 pl-8" onClick={() => handlePageSelect('Event', 'CreateEvent')} onKeyDown={(event) => handleKeyDown(event, () => handlePageSelect('Event', 'CreateEvent'))} tabIndex="0" role="button" aria-label="Create Event">
+  <FaCalendarCheck className={`text-white mr-2 ${activeSubPage === 'CreateEvent' ? 'text-blue-500' : ''}`} />
+  {!isCollapsed && <p className={`text-white ${activeSubPage === 'CreateEvent' ? 'text-blue-500' : ''}`}>Create Event</p>}
 </div>
-<div className={`cursor-pointer flex items-center w-full mb-2 hover:bg-gray-700 p-4 pl-8 ${activePage === 'EditEvent' ? 'bg-gray-700 text-white border-l-4 border-yellow-500' : ''}`} onClick={() => handlePageSelect('EditEvent')} onKeyDown={(event) => handleKeyDown(event, () => handlePageSelect('EditEvent'))} tabIndex="0" role="button" aria-label="Edit Event">
-  <FaEdit className="text-white mr-2" />
-  {!isCollapsed && <p className="text-white">Edit Event</p>}
+
+
+<div className="cursor-pointer flex items-center w-full mb-2 hover:bg-gray-700 p-4 pl-8" onClick={() => handlePageSelect('Event', 'EditEvent')} onKeyDown={(event) => handleKeyDown(event, () => handlePageSelect('Event', 'EditEvent'))} tabIndex="0" role="button" aria-label="Edit Event">
+  <FaEdit className={`text-white mr-2 ${activeSubPage === 'EditEvent' ? 'text-yellow-500' : ''}`} />
+  {!isCollapsed && <p className={`text-white ${activeSubPage === 'EditEvent' ? 'text-yellow-500' : ''}`}>Edit Event</p>}
 </div>
+
+
+
         </>
       )}
     <div className={`cursor-pointer flex items-center w-full mb-2 hover:bg-gray-700 p-4 ${activePage === 'User' ? 'bg-gray-700 text-white border-l-4 border-indigo-500' : ''}`} onClick={() => handlePageSelect('User')} onKeyDown={(event) => handleKeyDown(event, () => handlePageSelect('User'))} tabIndex="0" role="button" aria-label="Users">
