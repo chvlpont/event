d989a7dca9f6644b22451a5879847d32bec23219
oskarchr <oskarchr7@gmail.com>
2024-05-13 17:07:37 +0200

    Added redirect button when signed in

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import { ModeToggle } from "@/components/mode-toggle"
 import { Button } from "@/components/ui/button"
 import { SignInButton, SignUpButton, SignOutButton, SignedIn, SignedOut } from "@clerk/nextjs"
-// import Link from "next/link"
+import Link from "next/link"
 
 function LandingPage() {
   return (
@@ -28,13 +28,17 @@ function LandingPage() {
                 Sign out
               </Button>
             </SignOutButton>
-              {/* Länk här eller redirecta direkt när man loggat in?*/} 
           </SignedIn>
         </div>
       </header>
       
-      <div className="flex flex-col items-center mt-32 gap-4">
-        <h1 className="text-6xl font-bold">En jättebra rubrik</h1>
+      <div className="flex flex-col items-center gap-4">
+        <SignedIn>
+          <Button asChild className="mt-4">
+            <Link href="/admin">Enter dashboard</Link>
+          </Button>
+        </SignedIn>
+        <h1 className="text-6xl font-bold mt-32">En jättebra rubrik</h1>
         <p className="text-muted-foreground">en cool slogan</p>
         <img src="https://images.unsplash.com/photo-1622465911368-72162f8da3e2?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         className="w-1/2"></img>
