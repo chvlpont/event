0d5b617687ae00ea0a48ea363c951d963f97e94e
oskarchr <oskarchr7@gmail.com>
2024-05-20 17:46:24 +0200

    Added delete button

--- app/admin/_components/eventList.jsx
+++ app/admin/_components/eventList.jsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button"
 import { db } from "@/firebase.config"
-import { collection, onSnapshot, query } from "firebase/firestore"
+import { collection, deleteDoc, doc, onSnapshot, query } from "firebase/firestore"
 import Link from "next/link"
 import { useEffect, useState } from "react"
 
@@ -25,6 +25,17 @@ function EventList() {
   
     return () => unsub();
   }, []);
+
+  const deleteEvent = async (eventId) => {
+    try {
+      const eventRef = doc(db, "events", eventId);
+      await deleteDoc(eventRef);
+      console.log("Event deleted successfully!");
+    } catch (error) {
+      console.error("Error deleting event:", error);
+      throw error;
+    }
+  }
   
   if (loading) return <div>Loading...</div>
 
@@ -43,18 +54,23 @@ function EventList() {
       <div key={event.id} className={`bg-white dark:bg-gray-800 p-4 shadow-lg grid grid-cols-7 items-center ${index === eventList.length - 1 ? 'rounded-b-lg' : ''}`}>
         <img src={event.image} alt="Event" className="w-16 h-16 rounded-full shadow col-span-1"/>
         <h2 className="text-lg font-semibold col-span-1">{event.title}</h2>
-        <p className="col-span-1">{event.date}</p>
+        <p className="col-span-1">{event.date.replace("T", " ")}</p>
         <p className="col-span-1">{event.location}</p>
         <p className="line-clamp-2 col-span-1">{event.description}</p>
       <div className="flex items-center justify-center col-span-1">
         <span>{event.registrants}/{event.limit}</span>
         {event.registrants >= event.limit && <span className="ml-2 text-red-500">(Full)</span>}
       </div>
-      <Button asChild type="submit" className="w-full py-3 px-4 bg-blue-400 text-white dark:bg-blue-600 dark:text-gray-200 rounded hover:bg-blue-500 dark:hover:bg-blue-900 focus:outline-none active:scale-95 transition duration-200 ease-in-out col-span-1">
-        <Link href={`/admin/${event.id}`}>
-          <p>Edit</p>
-        </Link>
-      </Button>
+      <div className="flex justify-center gap-2">
+        <Button asChild type="submit" className="w-full h-full py-3 px-4 bg-blue-400 text-white dark:bg-blue-600 dark:text-gray-200 rounded hover:bg-blue-500 dark:hover:bg-blue-900 focus:outline-none active:scale-95 transition duration-200 ease-in-out col-span-1">
+          <Link href={`/admin/${event.id}`}>
+            <p>Edit</p>
+          </Link>
+        </Button>
+        <Button variant="destructive" onClick={() => deleteEvent(event.id)} className="">
+          <p>Remove</p>
+        </Button>
+      </div>
       </div>
     ))}
     </div>
