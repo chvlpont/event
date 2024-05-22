4a86f3bf7137d72e0603133f46caec465290bb66
oskarchr <oskarchr7@gmail.com>
2024-05-22 15:37:47 +0200

    Added edit event functionality to communicate with database

--- app/admin/event/page.jsx
+++ app/admin/event/page.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import EventList from '../_components/eventList';
 import { FaSearch } from 'react-icons/fa';
+import Link from 'next/link';
 
 
 
@@ -15,9 +16,11 @@ const Event = () => {
       <div className="p-5">
       <h2 className="text-3xl font-bold mb-5">Event Page</h2>
       <div className="flex flex-col lg:flex-row justify-between items-center mb-5">
-        <button className="py-3 px-4 bg-blue-400 text-white dark:bg-blue-600 dark:text-gray-200 rounded hover:bg-blue-500 dark:hover:bg-blue-900 focus:outline-none active:scale-95 transition duration-200 ease-in-out mb-4 lg:mb-0">
-          Create Event
-        </button>
+        <Link href="/admin/event/createEvent">
+          <button className="py-3 px-4 bg-blue-400 text-white dark:bg-blue-600 dark:text-gray-200 rounded hover:bg-blue-500 dark:hover:bg-blue-900 focus:outline-none active:scale-95 transition duration-200 ease-in-out mb-4 lg:mb-0">
+            Create Event
+          </button>
+        </Link>
         <div className="relative w-full lg:w-10/12 xl:w-8/12">
         <input 
    className="shadow appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 bg-gray-800 leading-tight focus:outline-none focus:shadow-outline"  
