13305d7aaec7216d24c4572468d1b5b6b7a19ecd
cvhlpont <pontchvl@gmail.com>
2024-05-30 17:47:47 +0200

    update: changed website name

--- app/_components/landing-page.jsx
+++ app/_components/landing-page.jsx
@@ -1,33 +1,42 @@
-'use client'
-import React, { useEffect, useState } from 'react';
-import { ModeToggle } from "@/components/mode-toggle"
-import { Button } from "@/components/ui/button"
-import { SignInButton, SignUpButton, SignOutButton, SignedIn, SignedOut } from "@clerk/nextjs"
-import Link from "next/link"
+"use client";
+import React, { useEffect, useState } from "react";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import {
+  SignInButton,
+  SignUpButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+import Link from "next/link";
 import { FaArrowCircleRight } from "react-icons/fa";
-import { getLandingPageContent } from '@/utils/eventservices';
-
-
+import { getLandingPageContent } from "@/utils/eventservices";
 
 function LandingPage() {
-const [content, setContent] = useState({ title: '', content: '', imageUrl: '' });
-
-
-
-useEffect(() => {
-  getLandingPageContent().then(data => {
-    if (data) {
-      setContent({ title: data.title, content: data.content, imageUrl: data.imageUrl });
-    }
+  const [content, setContent] = useState({
+    title: "",
+    content: "",
+    imageUrl: "",
   });
-}, []);
 
+  useEffect(() => {
+    getLandingPageContent().then((data) => {
+      if (data) {
+        setContent({
+          title: data.title,
+          content: data.content,
+          imageUrl: data.imageUrl,
+        });
+      }
+    });
+  }, []);
 
   return (
     <>
       <header className="w-full border-b">
         <div className="container flex p-2 justify-between gap-2">
-          <p className="flex-1 text-2xl font-bold">BackOffice | *companyName*</p>
+          <p className="flex-1 text-2xl font-bold">BackOffice | Event</p>
           <ModeToggle />
           <SignedOut>
             <SignInButton mode="modal">
@@ -50,22 +59,22 @@ useEffect(() => {
           </SignedIn>
         </div>
       </header>
-      
+
       <div className="flex flex-col items-center gap-4">
         <SignedIn>
           <Button asChild className="flex mt-10 absolute h-12 gap-2">
             <Link href="/admin">
               <p className="text-lg">Enter dashboard</p>
-              <FaArrowCircleRight className=""/>
+              <FaArrowCircleRight className="" />
             </Link>
           </Button>
         </SignedIn>
         <h1 className="text-6xl font-bold mt-32">{content.title}</h1>
-<p className="text-muted-foreground">{content.content}</p>
-<img src={content.imageUrl} className="w-1/2"></img>
+        <p className="text-muted-foreground">{content.content}</p>
+        <img src={content.imageUrl} className="w-1/2"></img>
       </div>
     </>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
